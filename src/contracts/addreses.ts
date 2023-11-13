24f82a555d2bf740bb40e881c3ab210b72f779d4
Patrick Gallagher <blockchainbuddha@gmail.com>
2023-11-13 15:52:56 +0300

    add sepolia subgraph

--- src/contracts/addreses.ts
+++ src/contracts/addreses.ts
@@ -322,7 +322,7 @@ const tokens: Record<GebDeployment, TokenList> = {
 }
 
 const subgraphs: Record<GebDeployment, string> = {
-    'arbitrum-sepolia': '',
+    'arbitrum-sepolia': 'https://api.studio.thegraph.com/query/52770/open-dollar---testnet/v1.5.8-rc.1',
     'arbitrum-goerli': 'https://api.studio.thegraph.com/query/52770/open-dollar---testnet/v1.5.6-rc.1',
     arbitrum: '',
 }
